1b5dd860d23f41cc8fb4d1046cf20de824cf7860
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2022-10-02 15:41:51 +0100

    Create console-creature.cs

---
+++ console-creature.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // Draws a ASCII art ghost in terminal
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| 0 |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("- Gavin the Ghost");    
+     }
+  }
+}
