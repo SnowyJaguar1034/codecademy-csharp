17dd1d1c83e762dd0dc4d2a56cdbab9011c39c7d
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2022-10-02 15:50:03 +0100

    Create data-types-and-varibales.cs

---
+++ data-types-and-varibales.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Review
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /* use this space to write your own short program! 
+      Here's what you learned:
+
+      DATA TYPES: int, double, char, string, bool
+      VARIABLES: datatype variableName = value;
+      COMMON ERRORS: wrong type, wrong value, no semicolon
+      DATA TYPE CONVERSION: implicit, explicit, methods
+
+      Good luck! */
+      int myint = 700;
+      double mydouble = 3090.567;
+      char mychar = "l";
+      string mystring = "Felicity Grey looked at the crumpled map in her hands and felt anxious.\n\nShe walked over to the window and reflected on her deprived surroundings. She had always loved damp Cardiff with its queenlike, quickest quarries. It was a place that encouraged her tendency to feel anxious.\n\nThen she saw something in the distance, or rather someone. It was the figure of Maureen Cockle. Maureen was a delightful hero with greasy arms and ample lips.\n\nFelicity gulped. She glanced at her own reflection. She was a hungry, stingy, cocoa drinker with sloppy arms and blonde lips. Her friends saw her as a dry, depressed dolphin. Once, she had even helped a cold toddler recover from a flying accident.\n\nBut not even a hungry person who had once helped a cold toddler recover from a flying accident, was prepared for what Maureen had in store today.\n\nThe sleet rained like partying mice, making Felicity irritable.\n\nAs Felicity stepped outside and Maureen came closer, she could see the cloudy glint in her eye.\n\n\"Look Felicity,\" growled Maureen, with a mean glare that reminded Felicity of delightful guppies. \"It's not that I don't love you, but I want peace. You owe me 1018 gold pieces.\"\n\nFelicity looked back, even more irritable and still fingering the crumpled map. \"Maureen, what's up Doc,\" she replied.\n\nThey looked at each other with stressed feelings, like two mangled, modern monkeys running at a very caring funeral, which had jazz music playing in the background and two delightful uncles walking to the beat.\n\nSuddenly, Maureen lunged forward and tried to punch Felicity in the face. Quickly, Felicity grabbed the crumpled map and brought it down on Maureen's skull.\n\nMaureen's greasy arms trembled and her ample lips wobbled. She looked ecstatic, her wallet raw like a helpless, hard hat.\n\nThen she let out an agonising groan and collapsed onto the ground. Moments later Maureen Cockle was dead.\n\nFelicity Grey went back inside and made herself a nice mug of cocoa.\n\nTHE END";
+      bool mybool = true;
+
+
+
+    }
+  }
+}
