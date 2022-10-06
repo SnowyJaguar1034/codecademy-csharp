1a88adfc7572b395d3445e5caec82874d3ca5de3
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2022-10-06 17:12:48 +0100

    Update mad-libs.cs

--- C#/mad-libs.cs
+++ C#/mad-libs.cs
@@ -1,1 +1,73 @@
+using System;
 
+namespace MadLibs
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+      Description & Author of the program:
+      */
+      string description = "This program creates a unique Mad Libs story from the users inputs.";
+      string author = "Teagan Collyer";
+
+      // Let the user know that the program is starting:
+      Console.WriteLine($"{author}'s Mad Libs generator is starting! {description}");
+
+      // Give the Mad Lib a title:
+      Console.WriteLine("Give the Mad Lib a title: ");
+      string title = Console.ReadLine();
+
+      Console.WriteLine($"You gave the Mad Lib a title of: {title}");
+
+      // Define user input and variables:
+      Console.WriteLine("Name your main character: ");
+      string name = Console.ReadLine();
+
+      Console.WriteLine("Now it's time to enter your three adjectives.\nAn adjective is a word that describes a noun, like a color (‘blue’), or feeling (‘silly’), texture (‘soft’).");
+      Console.Write("First adjective: ");
+      string adj1 = Console.ReadLine();
+
+      Console.Write("Second adjective: ");
+      string adj2 = Console.ReadLine();
+
+      Console.Write("Third adjective: ");
+      string adj3 = Console.ReadLine();
+
+      Console.WriteLine("Now it's time to enter your verb.\nA verb is a word that represents an action, like ‘sit’, ‘eat’, ‘sleep’.");
+      Console.WriteLine("Verb: ");
+      string verb = Console.ReadLine();
+
+      Console.WriteLine("Now it's time to enter your two nouns.\nA noun is a person (‘girl’), place (‘cabin’), or thing (‘toaster’).");
+      Console.WriteLine("First noun: ");
+      string noun1 = Console.ReadLine();
+
+      Console.WriteLine("Second noun: ");
+      string noun2 = Console.ReadLine();
+      
+      Console.WriteLine("Now it's time for more general random inputs.");
+      Console.WriteLine("A type of animal: ");
+      string animal = Console.ReadLine();
+      Console.WriteLine("A type of main course: ");
+      string food = Console.ReadLine();
+      Console.WriteLine("A type of fruit: ");
+      string fruit = Console.ReadLine();
+      Console.WriteLine("A superhero: ");
+      string superhero = Console.ReadLine();
+      Console.WriteLine("A country name: ");
+      string country = Console.ReadLine();
+      Console.WriteLine("A type of dessert: ");
+      string dessert = Console.ReadLine();
+      Console.WriteLine("A year: ");
+      string year = Console.ReadLine();
+
+      // The template for the story:
+
+      string story = $"This morning {name} woke up feeling {adj1}. 'It is going to be a {adj2} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adj3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.";
+
+      // Print the story:
+      Console.WriteLine(title + "\n\n" + story);
+    }
+  }
+}
