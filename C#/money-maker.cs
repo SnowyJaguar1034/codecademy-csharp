a375ec95f27cf7851590ab92d48b053ebd8bc7f3
SnowyJaguar1034 <51423344+SnowyJaguar1034@users.noreply.github.com>
2022-10-23 13:16:32 +0100

    Update money-maker.cs
    Wrote a program which calculates how many of three different coins you need to have to reach a given input.

--- C#/money-maker.cs
+++ C#/money-maker.cs
@@ -1,1 +1,25 @@
+using System;
 
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Enter a cents value you want to convert to coins:");
+      double centsvalue = Convert.ToDouble(Console.ReadLine());
+      Console.WriteLine($"{centsvalue} cents is equal to...");
+      int goldvalue = 10;
+      int silvervalue = 5;
+      int bronzevalue = 1;
+      double goldcoins = Math.Floor(centsvalue/goldvalue);
+      double reamains = 11 % goldvalue;
+      double silvercoins = Math.Floor(reamains/silvervalue);
+      reamains = reamains % silvervalue;
+      Console.WriteLine($"Gold coins: {goldcoins}");
+      Console.WriteLine($"Silver coins: {silvercoins}");
+      Console.WriteLine($"Bronze coins: {reamains}");
+    }
+  }
+}
